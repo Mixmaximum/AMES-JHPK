7cba464cd8a207d565c6d1f8e082fb249d5d6ff6
ZettaSlow <26.caleb.guzman@ksd.org>
2025-03-26 08:51:12 -0700

    Player Prefab, Player attack Changes

--- AMES Project/Assets/K/Scripts/Handlers/DataHandler.cs
+++ AMES Project/Assets/K/Scripts/Handlers/DataHandler.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 
 public class DataHandler : MonoBehaviour
 {
-    public float timeMultiplier = 1f;
+    public float timeMultiplier = 1f; // this is the variable that controls the time that things like enemies go by
+    public float timeSlowDuration = 7f;
 
     public IEnumerator SlowTime()
     {
         Debug.Log("Time Slowed");
-        timeMultiplier = 0.5f;
-        yield return new WaitForSeconds(10f);
+        timeMultiplier = 0.5f; // since enemy speed and whatnot is multiplied by this variable, it slows down independently of the actual time.timescale
+        yield return new WaitForSeconds(timeSlowDuration);
         timeMultiplier = 1f;
         Debug.Log("Time Unslowed");
     }
+
+    // having an independent multiplier that handles the time individually of the time.timescale variable also has the added
+    // bonus of having more compatability with things like a main menu
+    // as you don't have to worry about the time unslowing down when you pause and unpause
 }
