dd09255bd85ac0c23f38f834b1dbf8e5d5351695
Mixmaximum <mixmaximum752@gmail.com>
2025-03-26 08:46:25 -0700

    added death bool

--- AMES Project/Assets/HT folder/Scripts/PlayerHealth.cs
+++ AMES Project/Assets/HT folder/Scripts/PlayerHealth.cs
@@ -1,12 +1,24 @@
 using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] GameObject cam;
+
+    [Header("Health Settings")]
     [SerializeField] float health;
     [SerializeField] float healAmount;
     [SerializeField] float timeToHeal;
+    [Space(5)]
+
+    [Header("I-Frame Settings")]
+    [SerializeField] float invincibilityDuration = 2f;
+    [Space(5)]
+
+    float invincibilityTimeRemaining = 0f;
+    bool isInvincible = false;
     float healTimer;
     float maxHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -35,16 +47,40 @@ public class PlayerHealth : MonoBehaviour
     }
     public void TakeDamage(float damage)
     {
-        health -= damage;
-        if (health <= 0)
+        if (!isInvincible) 
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            health -= damage;
+            isInvincible = true;
+            invincibilityTimeRemaining = invincibilityDuration;
+            if (health <= 0)
+            {
+                Die();
+            }
+            healTimer = timeToHeal;
         }
-        healTimer = timeToHeal;
     }
 
     public void Heal(float healing) 
     {
         health += healing;
+    }
+
+    public void Die()
+    {
+        cam.GetComponent<Animator>().SetBool("Dead", true);
+    }
+    private void HandleIFrames()
+    {
+        if (isInvincible)
+        {
+            invincibilityTimeRemaining -= Time.deltaTime;
+
+            if (invincibilityTimeRemaining <= 0)
+            {
+                isInvincible = false;
+                //Debug.Log("No longer invincible");
+            }
+        }
+   
     }
 }
