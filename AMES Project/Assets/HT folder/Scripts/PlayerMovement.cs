a1dca1adb9d1804af7f14543732513cfc4edb1dc
Dman7713 <PeytonPrivratsky@gmail.com>
2025-03-28 11:21:40 -0700

    Mr Adelmund is the nicest teacher ever.

--- AMES Project/Assets/HT folder/Scripts/PlayerMovement.cs
+++ AMES Project/Assets/HT folder/Scripts/PlayerMovement.cs
@@ -3,92 +3,70 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     float playerHeight = 2f;
+
     [Header("References")]
     [SerializeField] Transform orientation;
     [SerializeField] WallRun wallrun;
-    [SerializeField] Animator animator; // Added Animator reference
-    [Space(5)]
+    [SerializeField] Animator animator;
+    [SerializeField] Transform groundCheck;
 
     [Header("Movement")]
     [SerializeField] float moveSpeed = 6f;
     [SerializeField] float movementMultiplier = 10f;
     [SerializeField] float airMovementMultiplier = 0.4f;
-    [Space(5)]
 
     [Header("Sprinting")]
     [SerializeField] public float walkSpeed = 4f;
     [SerializeField] public float sprintSpeed = 6f;
     [SerializeField] float acceleration = 10f;
-    [Space(5)]
 
     [Header("Keybinds")]
-    [SerializeField] KeyCode jumpkey = KeyCode.Space;
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
     [SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
     [SerializeField] KeyCode crouchKey = KeyCode.LeftControl;
     [SerializeField] KeyCode slideKey = KeyCode.LeftControl;
-    [Space(5)]
 
-    [Header("Jumping/ground")]
+    [Header("Jumping/Ground")]
     [SerializeField] float jumpForce = 5f;
     [SerializeField] float groundDistance = 0.4f;
-    [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask ground;
-    [Space(5)]
 
     [Header("Drag/Gravity")]
     [SerializeField] float groundDrag = 6f;
     [SerializeField] float airDrag = 2f;
     [SerializeField] float fallingGrav;
-    [Space(5)]
 
-    [Header("Crouch settings")]
+    [Header("Crouch Settings")]
     [SerializeField] private Vector3 crouchHeight;
     [SerializeField] private float crouchMoveSpeed;
     [SerializeField] private float crouchSprintSpeed;
     [SerializeField] private float crouchFloorDetectDist;
-    [Space(5)]
 
-    [Header("Slide settings")]
-    [SerializeField] float requiredSlideSpeed; // how fast you have to be to start sliding
+    [Header("Slide Settings")]
+    [SerializeField] float requiredSlideSpeed;
     [SerializeField] float slideLength;
     [SerializeField] float slideForce;
-    [Space(5)]
 
     float horizontalMovement;
     float verticalMovement;
     float currentVelocity;
     float currentSlideSpeed;
+    float slideTimer;
+
     bool isGrounded;
     bool isCrouching;
     bool isSliding;
     bool ableToCrouch;
+    bool isJumping;
+
     int impulseCounter;
-    int slideImpulseCounter;
-    float slideTimer;
-    Rigidbody rb;
 
     Vector3 moveDirection;
     Vector3 slopeMoveDirection;
     Vector3 slideDirection;
 
     RaycastHit slopeHit;
-
-    private bool OnSlope()
-    {
-        // use a raycast to detect if player is on a slope
-        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
-        {
-            if (slopeHit.normal != Vector3.up)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        return false;
-    }
+    Rigidbody rb;
 
     private void Start()
     {
@@ -98,7 +76,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        //checks for ground
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, ground);
         ableToCrouch = Physics.CheckSphere(groundCheck.position, crouchFloorDetectDist, ground);
 
@@ -109,118 +86,84 @@ public class PlayerMovement : MonoBehaviour
         StartSlide();
 
         if (isSliding)
-        {
             SlideMovement();
-        }
 
-        // Stop slide when slide key is released
         if (isSliding && Input.GetKeyUp(slideKey))
-        {
             StopSlide();
-        }
 
         Crouch();
 
         currentVelocity = rb.linearVelocity.magnitude;
-        //Debug.Log(currentVelocity);
 
-        //Jumps
-        if (Input.GetKeyDown(jumpkey) && isGrounded)
-        {
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
             Jump();
-        }
 
-        // adjusts slope move direction to be slightly upwards
         slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
 
-        // Animation control
         UpdateAnimations();
-    }
-
-    void MyInput()
-    {
-        //Gets move inputs
-        horizontalMovement = Input.GetAxisRaw("Horizontal");
-        verticalMovement = Input.GetAxisRaw("Vertical");
-
-        moveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
-    }
-
-    private void Jump()
-    {
-        if (isGrounded) //checks groundedness
-        {
-            // has player jump forwards
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
-            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-        }
-    }
-
-    private void ControlSpeed()
-    {
-        // changes movespeed between sprint and walk over time if the player is grounded
-        if (Input.GetKey(sprintKey) && isGrounded && !isCrouching)
-        {
-            moveSpeed = Mathf.Lerp(moveSpeed, sprintSpeed, acceleration * Time.deltaTime);
-        }
-        else if (isCrouching && !Input.GetKey(sprintKey))
-        {
-            moveSpeed = Mathf.Lerp(moveSpeed, crouchMoveSpeed, acceleration * Time.deltaTime);
-        }
-        else if (Input.GetKey(sprintKey) && isCrouching)
-        {
-            moveSpeed = Mathf.Lerp(moveSpeed, crouchSprintSpeed, acceleration * Time.deltaTime);
-        }
-        else
-        {
-            moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, acceleration * Time.deltaTime);
-        }
-    }
-
-    void ControlDrag()
-    {
-        // changes drag amount if the player is grounded or not
-        if (isGrounded)
-        {
-            rb.linearDamping = groundDrag;
-        }
-        else
-        {
-            rb.linearDamping = airDrag;
-        }
     }
 
     private void FixedUpdate()
     {
-        MovePLayer();
+        MovePlayer();
     }
 
-    void MovePLayer()
+    void MyInput()
     {
-        // moves the player based on move direction and speed
-        if (isGrounded && !OnSlope() && !isSliding)
-        {
-            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
-        }
-        // moves the player based on sloped move direction and speed
-        else if (isGrounded && OnSlope() && !isSliding)
-        {
-            rb.AddForce(slopeMoveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
-        }
-        // moves the player slower while in the air
-        else if (!isGrounded && !isSliding)
-        {
-            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier * airMovementMultiplier, ForceMode.Acceleration);
-        }
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+        moveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
     }
 
-    // Make the player fall heavier
+    void MovePlayer()
+    {
+        if (isGrounded && !OnSlope() && !isSliding)
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        else if (isGrounded && OnSlope() && !isSliding)
+            rb.AddForce(slopeMoveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        else if (!isGrounded && !isSliding)
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier * airMovementMultiplier, ForceMode.Acceleration);
+    }
+
+    bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
+            return slopeHit.normal != Vector3.up;
+        return false;
+    }
+
+    void Jump()
+    {
+        if (!isGrounded) return;
+
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+        isJumping = true;
+        animator.SetBool("IsJumping", true);
+    }
+
+    void ControlSpeed()
+    {
+        if (Input.GetKey(sprintKey) && isGrounded && !isCrouching)
+            moveSpeed = Mathf.Lerp(moveSpeed, sprintSpeed, acceleration * Time.deltaTime);
+        else if (isCrouching && !Input.GetKey(sprintKey))
+            moveSpeed = Mathf.Lerp(moveSpeed, crouchMoveSpeed, acceleration * Time.deltaTime);
+        else if (Input.GetKey(sprintKey) && isCrouching)
+            moveSpeed = Mathf.Lerp(moveSpeed, crouchSprintSpeed, acceleration * Time.deltaTime);
+        else
+            moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, acceleration * Time.deltaTime);
+    }
+
+    void ControlDrag()
+    {
+        rb.linearDamping = isGrounded ? groundDrag : airDrag;
+    }
+
     void FallingGrav()
     {
         if (!isGrounded && !wallrun.wallRunning)
-        {
             rb.AddForce(Vector3.down * fallingGrav, ForceMode.Force);
-        }
     }
 
     void Crouch()
@@ -231,23 +174,22 @@ public class PlayerMovement : MonoBehaviour
             isCrouching = true;
             if (impulseCounter < 1)
             {
-                rb.AddForce(Vector3.down * 5, ForceMode.Impulse);
+                rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
                 impulseCounter = 1;
             }
         }
         else if (!isSliding)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(1f, 1f, 1f);
             isCrouching = false;
             impulseCounter = 0;
         }
     }
 
-    private void StartSlide()
+    void StartSlide()
     {
         if (Input.GetKeyDown(slideKey) && !isCrouching && currentVelocity >= requiredSlideSpeed && !isSliding && isGrounded)
         {
-            // Check if the player is touching an object on the "Ground" layer
             if (Physics.Raycast(groundCheck.position, Vector3.down, groundDistance, ground))
             {
                 slideDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
@@ -259,42 +201,34 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void SlideMovement()
+    void SlideMovement()
     {
         currentSlideSpeed = Mathf.Lerp(currentSlideSpeed, 0, slideTimer * Time.deltaTime);
-        Debug.Log("Slide force = " + slideDirection.normalized * slideForce);
         rb.AddForce(slideDirection.normalized * currentSlideSpeed, ForceMode.Force);
 
-        if (Input.GetKeyDown(jumpkey) || Input.GetKeyUp(slideKey))
-        {
+        if (Input.GetKeyDown(jumpKey) || Input.GetKeyUp(slideKey))
             StopSlide();
-        }
     }
 
-    private void StopSlide()
+    void StopSlide()
     {
         isSliding = false;
-        transform.localScale = new Vector3(1, 1, 1);
+        transform.localScale = new Vector3(1f, 1f, 1f);
     }
 
-    // Animation methods
     void UpdateAnimations()
     {
-        // Set Speed parameter based on current velocity
         animator.SetFloat("Speed", currentVelocity);
+        animator.speed = Mathf.Clamp(currentVelocity / 5f, 0.5f, 2f);
 
-        // Check if player is grounded or in the air to handle jumping animation if needed
         animator.SetBool("IsGrounded", isGrounded);
+        animator.SetBool("IsFalling", !isGrounded);
 
-        // Set IsFalling animation bool if the player is not grounded
-        if (!isGrounded)
+        // Reset jump animation when grounded again
+        if (isJumping && isGrounded)
         {
-            animator.SetBool("IsFalling", true); // Start falling animation when not grounded
-        }
-        else
-        {
-            animator.SetBool("IsFalling", false); // Stop falling animation when grounded
+            animator.SetBool("IsJumping", false);
+            isJumping = false;
         }
     }
-
 }
